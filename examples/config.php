2701fc6d6890cdee2bc3f6d96d9dc8ad24bac362
pmill <dev.pmill@gmail.com>
2016-09-01 19:08:36 +0100

    Fixed error checking

--- examples/config.php
+++ examples/config.php
@@ -5,7 +5,7 @@ $classLoader = new SplClassLoader('pmill\Plesk', '../../src');
 $classLoader->register();
 
 $config = array(
-    'host'=>'plesk12-webhost.demo.parallels.com',
+    'host'=>'plesk12-webadmin.demo.parallels.com',
     'username'=>'admin',
     'password'=>'panel',
 );
