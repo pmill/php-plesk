541dfbe5e617f4ca4966e3e0586898f27588735c
mUniKeS <munikes@members.fsf.org>
2016-11-07 18:16:20 +0100

    Add throw the Api Execption

--- src/pmill/Plesk/BaseRequest.php
+++ src/pmill/Plesk/BaseRequest.php
@@ -172,11 +172,13 @@ EOT;
 
                 return $this->processResponse($responseXml);
             }
+            return false;
+
         } catch (ApiRequestException $e) {
             $this->error = $e;
+            throw new ApiRequestException($e->getMessage(), $e->getCode());
         }
 
-        return false;
     }
 
     /**
