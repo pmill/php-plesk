83ed32ee9775cf181c3c98a712877d65ac0e31b8
mUniKeS <munikes@members.fsf.org>
2016-11-14 13:12:24 +0100

    Add list empty subscription and sites

--- src/pmill/Plesk/ListSubscriptions.php
+++ src/pmill/Plesk/ListSubscriptions.php
@@ -33,34 +33,36 @@ EOT;
 
         for ($i = 0; $i < count($xml->webspace->get->result); $i++) {
             $webspace = $xml->webspace->get->result[$i];
+            if (isset($webspace->id)){
 
-            $hosting = [];
-            foreach ($webspace->data->hosting->children() as $host) {
-                $hosting[$host->getName()] = Xml::getProperties($host);
-            }
+                $hosting = [];
+                foreach ($webspace->data->hosting->children() as $host) {
+                    $hosting[$host->getName()] = Xml::getProperties($host);
+                }
 
-            $subscriptions = [];
-            foreach ($webspace->data->subscriptions->children() as $subscription) {
-                $subscriptions[] = [
-                    'locked' => (bool)$subscription->locked,
-                    'synchronized' => (bool)$subscription->synchronized,
-                    'plan-guid' => (string)$subscription->plan->{"plan-guid"},
+                $subscriptions = [];
+                foreach ($webspace->data->subscriptions->children() as $subscription) {
+                    $subscriptions[] = [
+                        'locked' => (bool)$subscription->locked,
+                        'synchronized' => (bool)$subscription->synchronized,
+                        'plan-guid' => (string)$subscription->plan->{"plan-guid"},
+                    ];
+                }
+
+                $result[] = [
+                    'id' => (string)$webspace->id,
+                    'status' => (string)$webspace->status,
+                    'subscription_status' => (int)$webspace->data->gen_info->status,
+                    'created' => (string)$webspace->data->gen_info->cr_date,
+                    'name' => (string)$webspace->data->gen_info->name,
+                    'owner_id' => (string)$webspace->data->gen_info->{"owner-id"},
+                    'hosting' => $hosting,
+                    'real_size' => (int)$webspace->data->gen_info->real_size,
+                    'dns_ip_address' => (string)$webspace->data->gen_info->dns_ip_address,
+                    'htype' => (string)$webspace->data->gen_info->htype,
+                    'subscriptions' => $subscriptions,
                 ];
             }
-
-            $result[] = [
-                'id' => (string)$webspace->id,
-                'status' => (string)$webspace->status,
-                'subscription_status' => (int)$webspace->data->gen_info->status,
-                'created' => (string)$webspace->data->gen_info->cr_date,
-                'name' => (string)$webspace->data->gen_info->name,
-                'owner_id' => (string)$webspace->data->gen_info->{"owner-id"},
-                'hosting' => $hosting,
-                'real_size' => (int)$webspace->data->gen_info->real_size,
-                'dns_ip_address' => (string)$webspace->data->gen_info->dns_ip_address,
-                'htype' => (string)$webspace->data->gen_info->htype,
-                'subscriptions' => $subscriptions,
-            ];
         }
 
         return $result;
