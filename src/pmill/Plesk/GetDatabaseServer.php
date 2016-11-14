dd63591253126a0cf0834f4a29b4c186b71755c8
mUniKeS <munikes@members.fsf.org>
2016-11-14 13:57:02 +0100

    Add a class GetDatabaseServer

---
+++ src/pmill/Plesk/GetDatabaseServer.php
@@ -0,0 +1,60 @@
+<?php
+namespace pmill\Plesk;
+
+class GetDatabaseServer extends BaseRequest
+{
+    /**
+     * @var string
+     */
+    public $xml_packet = <<<EOT
+<?xml version="1.0"?>
+<packet version="1.6.7.0">
+<db_server>
+   <get-local>
+      <filter>
+          {FILTER}
+      </filter>
+   </get-local
+</dd_server>
+</packet>
+EOT;
+
+    /**
+     * @var array
+     */
+    protected $default_params = [
+        'filter' => null,
+    ];
+
+    /**
+     * GetDatabaseServer constructor.
+     * @param array $config
+     * @param array $params
+     */
+    public function __construct(array $config, $params = [])
+    {
+        if (isset($params['type'])) {
+            $params['filter'] = new Node('type', $params['type']);
+        }
+        if (isset($params['id'])) {
+            $params['filter'] = new Node('id', $params['id']);
+        }
+        parent::__construct($config, $params);
+    }
+
+    /**
+     * @param $xml
+     * @return bool
+     * @throws ApiRequestException
+     */
+    protected function processResponse($xml)
+    {
+        if ($xml->db_server->{'get-local'}->result->status == 'error') {
+            throw new ApiRequestException($xml->db_server->{'get-local'}->result);
+        }
+
+        $this->id = (int)$xml->db_server->{'get-local'}->result->id;
+        return true;
+    }
+
+}
